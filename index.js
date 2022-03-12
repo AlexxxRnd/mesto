48677cda17e0f48a4e10eda444781a060f590b14
Alexander Garbuzov <spajk73@ya.ru>
2022-03-12 14:00:19 +0300

    add like button active

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let editButton = document.querySelector(".profile__edit-btn");
 let closeButton = document.querySelector(".popup__close-btn");
+let likeButton = document.querySelector(".element__like-btn");
 let popupForm = document.querySelector(".popup");
-
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__subname");
 let nameInput = document.querySelector(".popup__input-name");
@@ -19,13 +19,23 @@ function closepopup() {
     popupForm.classList.remove('popup_opened');
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
+function formSubmitHandler(evt) {
+    evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     closepopup();
 }
 
+function addLike() {
+    if (likeButton.classList.contains("element__like-btn_active")) {
+        likeButton.classList.remove("element__like-btn_active");
+    }
+    else {
+        likeButton.classList.add("element__like-btn_active");
+    }
+}
+
+likeButton.addEventListener('click', addLike);
 editButton.addEventListener('click', showpopup);
 closeButton.addEventListener('click', closepopup);
 formElement.addEventListener('submit', formSubmitHandler); 
