3a8fe49e8920103026d76efcf2241df914a8e89f
Alexander Garbuzov <spajk73@ya.ru>
2022-03-13 22:14:54 +0300

    fix 1st rev

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 let editButton = document.querySelector('.profile__edit-btn');
 let closeButton = document.querySelector('.popup__close-btn');
-//let likeButton = document.querySelector('.element__like-btn');
 let popupForm = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__subname');
@@ -26,16 +25,6 @@ function formSubmitHandler(evt) {
     closePopup();
 }
 
-//function addLike() {
-//    if (likeButton.classList.contains('element__like-btn_active')) {
-//        likeButton.classList.remove('element__like-btn_active');
-//    }
-//    else {
-//        likeButton.classList.add('element__like-btn_active');
-//    }
-//}
-
-//likeButton.addEventListener('click', addLike);
 editButton.addEventListener('click', showPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
