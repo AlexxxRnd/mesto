c3fb0b9253957a5b8a7069a8dda3fc61df4c7864
Alexander Garbuzov <spajk73@ya.ru>
2022-03-13 21:29:28 +0300

    fix 1st rev v2

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
-let editButton = document.querySelector(".profile__edit-btn");
-let closeButton = document.querySelector(".popup__close-btn");
-let likeButton = document.querySelector(".element__like-btn");
-let popupForm = document.querySelector(".popup");
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__subname");
-let nameInput = document.querySelector(".popup__input-name");
-let jobInput = document.querySelector(".popup__input-job");
+let editButton = document.querySelector('.profile__edit-btn');
+let closeButton = document.querySelector('.popup__close-btn');
+//let likeButton = document.querySelector('.element__like-btn');
+let popupForm = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__subname');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 
-let formElement = document.querySelector(".popup__form");
+let formElement = document.querySelector('.popup__form');
 
-function showpopup() {
+function showPopup() {
     popupForm.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
-function closepopup() {
+function closePopup() {
     popupForm.classList.remove('popup_opened');
 }
 
@@ -23,19 +23,19 @@ function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    closepopup();
+    closePopup();
 }
 
-function addLike() {
-    if (likeButton.classList.contains("element__like-btn_active")) {
-        likeButton.classList.remove("element__like-btn_active");
-    }
-    else {
-        likeButton.classList.add("element__like-btn_active");
-    }
-}
+//function addLike() {
+//    if (likeButton.classList.contains('element__like-btn_active')) {
+//        likeButton.classList.remove('element__like-btn_active');
+//    }
+//    else {
+//        likeButton.classList.add('element__like-btn_active');
+//    }
+//}
 
-likeButton.addEventListener('click', addLike);
-editButton.addEventListener('click', showpopup);
-closeButton.addEventListener('click', closepopup);
+//likeButton.addEventListener('click', addLike);
+editButton.addEventListener('click', showPopup);
+closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
