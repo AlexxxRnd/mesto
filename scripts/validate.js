96095eff40fce3050dd5e5c1f56d154bb8fdb10a
Alexander Garbuzov <spajk73@ya.ru>
2022-04-05 18:05:42 +0300

    fix

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,9 +3,6 @@ const objValidation = {
     inputElement: '.popup__input',
     submitButtonSelector: '.popup__save-btn',
     inactiveButtonClass: 'popup__save-btn_disabled',
-
-    inputErrorClass: 'popup__input_type-error',
-    errorClass: 'popup__error_visible'
 };
 
 const showInputError = (formElement, inputElement, errorMessage) => {
