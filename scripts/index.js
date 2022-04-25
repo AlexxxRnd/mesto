bede0f2cb0544faa37d7e370b048bd84afb80866
Alexander Garbuzov <spajk73@ya.ru>
2022-04-25 08:43:42 +0300

    fix 1st rev

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import { Card } from './Card.js';
 import { FormValidator } from './FormValidator.js';
+import { initialCards } from './cards.js';
 
 const popupEditButton = document.querySelector('.profile__edit-btn');
 const popupAddButton = document.querySelector('.profile__add-btn');
@@ -8,7 +9,6 @@ const popupCurrentList = document.querySelectorAll('.popup');
 
 const popupEditForm = document.getElementById('popup_editProfile');
 const popupAddForm = document.getElementById('popup_addCard');
-const popupAddSavebtn = document.getElementById('popup_addCard_save-btn');
 
 const profileForm = document.querySelector('.popup__form');
 const formElementAdd = document.querySelector('.popup__form_add');
@@ -19,34 +19,6 @@ const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 const mestoInput = document.querySelector('.popup__input_type_mesto-name');
 const linkInput = document.querySelector('.popup__input_type_url');
-
-
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
 
 export function showPopup(popup) {
     popup.classList.add('popup_opened');
